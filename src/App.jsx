0332d22c29370ae96b5a18cb7fdc35c2ee76fef2
Chronowork1 <jianxie6464@gmail.com>
2019-10-25 02:18:39 -0700

    For now, not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,65 @@
-import React from 'react';
-import Design from "./Component/Design";
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Design />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-    </div>
-  );
+//Special function App 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Stopwatch</h1>
+        <Stopwatch status={false} runningTime={0}/>
+      </div>
+    )
+  }
+}
+
+class Stopwatch extends Component{
+  state = {
+    status: false,
+    runningTime: 0
+  }
+
+  handleClick = () => {
+    this.setState(state => {
+      if(state.status) {
+        clearInterval(this.timer);
+      }else{
+        const startTime = Date.now() - this.state.runningTime;
+        this.timer = setInterval(() => {
+          this.setState({
+            runningTime: Date.now() - startTime
+          })
+        })
+      }
+      return {
+        status: !state.status
+      };
+    })
+  }
+
+  handleReset = () => {
+    clearInterval(this.timer)
+    this.setState({
+      runningTime: 0,
+      running: false
+    })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    const {status, runningTime} = this.state;
+
+    return (
+      <div>
+        <p>{runningTime}</p>
+        <button>{status ? 'Stop': 'Start'}</button>
+        <button>Reset</button>
+      </div>
+    )
+  }
 }
 
 export default App;
